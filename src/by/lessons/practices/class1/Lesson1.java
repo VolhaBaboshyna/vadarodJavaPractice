f262d0dca405d90e15be0fe1770ce53ede02e5a1
VolhaBaboshyna <volha.baboshyna@gmail.com>
2026-05-06 20:00:12 +0300

    Первое практическое занятие

---
+++ src/by/lessons/practices/class1/Lesson1.java
@@ -0,0 +1,48 @@
+package by.lessons.practices.class1;
+
+import java.util.Scanner;
+
+public class Lesson1 {
+    public static void main(String[] args) {
+        /*
+        System.out.println("Task 1");
+        int a;
+        a = 88/(10+12);
+        System.out.println("a = " + a);
+        System.out.println("***********************************************");
+        */
+
+        /*
+        System.out.println("Task 2");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите x");
+        int x = sc.nextInt();
+        System.out.println("Введите y");
+        int y = sc.nextInt();
+        System.out.println("Введите z");
+        int z = sc.nextInt();
+        int result = x*(y*z);
+        System.out.println("result = " + result);
+        System.out.println("***********************************************");
+*/
+
+         /*
+        System.out.println("Task 3");
+        int b;
+        b = (213+258)/(217-60);
+        System.out.println("b = " + b);
+        System.out.println("***********************************************");
+        */
+
+        System.out.println("Task 4");
+        int q=5;
+        int w=2;
+        int c;
+        c=q++ * w--;
+        System.out.println("q = " + q);
+        System.out.println("w = " + w);
+        System.out.println("c= " + c);
+        System.out.println("***********************************************");
+
+    }
+}
---
+++ src/by/lessons/practices/class1/Lesson1_2.java
@@ -0,0 +1,52 @@
+package by.lessons.practices.class1;
+
+import java.util.Scanner;
+
+public class Lesson1_2 {
+    public static void main(String[] args) {
+        /*  System.out.println("Task 1");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите a");
+        int a = sc.nextInt();
+        System.out.println("Введите b");
+        int b = sc.nextInt();
+        int c;
+        if (a > b) {
+            c = a - b;
+            System.out.println("a > b. Результат = " + c);
+        } else if (a == b) {
+            c = a + b;
+            System.out.println("a == b Результат = " + c);
+        } else {
+            c = b - a;
+            System.out.println("a < b Результат = " + c);
+        }
+         */
+        /*
+        System.out.println("Task 3");
+        System.out.println("Введите a");
+        int a = sc.nextInt();
+        if (a%3==0) {
+            System.out.println("Число a делится на 3 без остатка");
+        } else {
+            System.out.println("Число a не делится на 3 без остатка");
+        }
+*/
+
+        System.out.println("Task 4");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите e");
+        int e = sc.nextInt();
+        if (e%3==0 && e%7==0) {
+            System.out.println("Число e делится на 3 и на 7 без остатка");
+        }
+        else if (e%3==0) {
+            System.out.println("Число e делится на 3 остатка");
+        }
+        else if (e%7==0)
+        {
+            System.out.println("Число e делится на 7 остатка");
+        }
+
+    }
+}
